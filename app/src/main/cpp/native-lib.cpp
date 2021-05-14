038863adc2c12a2e231d8e7c07580855167e0a0b
lk <18513075404@163.com>
2021-05-14 17:47:09 +0800

    null

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -16,23 +16,23 @@ JNIEXPORT jstring
 
 Java_com_xiaojian_lkplayer_LkPlayer_native_1startPlay(JNIEnv
                                                       *env,
-                                                      jobject thiz, jstring
+                                                      jobject _thiz, jstring
                                                       url_, jobject surface) {
 
-    const char *path = env->GetStringUTFChars(url_, 0);
+    const char *path = env->GetStringUTFChars(url_, nullptr);
 //总上下文
     avformat_network_init();
     AVFormatContext *formatContext = avformat_alloc_context();
     ANativeWindow *aNativeWindow = ANativeWindow_fromSurface(env, surface);
-    AVDictionary *options = NULL;
+    AVDictionary *options = nullptr;
     av_dict_set(&options, "timeout", "300000", 0);
 
-    int ret = avformat_open_input(&formatContext, path, NULL, &options);
+    int ret = avformat_open_input(&formatContext, path, nullptr, &options);
     if (ret) {
         return nullptr;
     }
     //获取流
-    avformat_find_stream_info(formatContext, NULL);
+    avformat_find_stream_info(formatContext, nullptr);
     int video_steam_idx = -1;//视频流id
     for (int i = 0; i < formatContext->nb_streams; ++i) {
         if (formatContext->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
@@ -45,14 +45,14 @@ Java_com_xiaojian_lkplayer_LkPlayer_native_1startPlay(JNIEnv
     const AVCodec *avCodec = avcodec_find_decoder(codecpar->codec_id);
     AVCodecContext *avCodecContext = avcodec_alloc_context3(avCodec);
     avcodec_parameters_to_context(avCodecContext, codecpar);
-    avcodec_open2(avCodecContext, avCodec, NULL);
+    avcodec_open2(avCodecContext, avCodec, nullptr);
     //解码yuv数据
     AVPacket *avPacket = av_packet_alloc();
     //从视频流中获取数据包
     SwsContext *swsContext = sws_getContext(avCodecContext->width, avCodecContext->height,
                                             avCodecContext->pix_fmt,
                                             avCodecContext->width, avCodecContext->height,
-                                            AV_PIX_FMT_RGBA, SWS_BILINEAR, 0, 0, 0);
+                                            AV_PIX_FMT_RGBA, SWS_BILINEAR, nullptr, nullptr, nullptr);
 
     ANativeWindow_setBuffersGeometry(aNativeWindow, avCodecContext->width, avCodecContext->height,
                                      WINDOW_FORMAT_RGBA_8888);
@@ -80,10 +80,10 @@ Java_com_xiaojian_lkplayer_LkPlayer_native_1startPlay(JNIEnv
 //非零   正在解码
             if (ret == 0) {
 
-                ANativeWindow_lock(aNativeWindow, &outBuffer, NULL);
+                ANativeWindow_lock(aNativeWindow, &outBuffer, nullptr);
 
                 //渲染
-                uint8_t *firstWindow = static_cast<uint8_t *>(outBuffer.bits);
+                auto *firstWindow = static_cast<uint8_t *>(outBuffer.bits);
                 //输入源
                 uint8_t *src_data = det_data[0];
                 //每一行有多少字节rgba
